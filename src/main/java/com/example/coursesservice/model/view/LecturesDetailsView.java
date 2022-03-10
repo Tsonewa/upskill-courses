5a135f8a2cc55386b6d3ac03c1e2ca8404ec67b9
Tsonewa <tsonewa@gmail.com>
2022-03-10 12:01:21 +0200

    add courses/lecture view models

--- src/main/java/com/example/coursesservice/model/view/LecturesDetailsView.java
+++ src/main/java/com/example/coursesservice/model/view/LecturesDetailsView.java
@@ -2,5 +2,54 @@ package com.example.coursesservice.model.view;
 
 public class LecturesDetailsView {
 
-    
+    private String resourceUrl;
+    private String description;
+    private String name;
+    private String lectorDescription;
+
+    public LecturesDetailsView() {
+    }
+
+    public LecturesDetailsView(String resourceUrl, String description, String name, String lectorDescription) {
+        this.resourceUrl = resourceUrl;
+        this.description = description;
+        this.name = name;
+        this.lectorDescription = lectorDescription;
+    }
+
+    public String getResourceUrl() {
+        return resourceUrl;
+    }
+
+    public LecturesDetailsView setResourceUrl(String resourceUrl) {
+        this.resourceUrl = resourceUrl;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LecturesDetailsView setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LecturesDetailsView setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLectorDescription() {
+        return lectorDescription;
+    }
+
+    public LecturesDetailsView setLectorDescription(String lectorDescription) {
+        this.lectorDescription = lectorDescription;
+        return this;
+    }
 }
